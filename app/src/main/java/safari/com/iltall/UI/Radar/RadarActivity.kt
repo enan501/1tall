b0ef1699e0664e9fcee3b8b74a237b56c43cde0c
SEJEONG_12 <mozzi981231@gmail.com>
2019-10-24 19:36:56 +0900

    마커이벤트만 하면댄당

--- app/src/main/java/safari/com/iltall/UI/Radar/RadarActivity.kt
+++ app/src/main/java/safari/com/iltall/UI/Radar/RadarActivity.kt
@@ -1,21 +1,137 @@
 package safari.com.iltall.UI.Radar
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
+import com.google.android.gms.maps.model.LatLng
+
+
+import kotlinx.android.synthetic.main.activity_radar.*
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 import safari.com.iltall.R
 
 class RadarActivity : AppCompatActivity() {
+
+    val LOCATION_REQUEST = 4444
+    lateinit var mapView:MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_radar)
-
-        var img = findViewById(R.id.splashimg) as ImageView
-        var gifimg = GlideDrawableImageViewTarget(img)
-        Glide.with(this).load(R.drawable.radar_sweep).into(gifimg)
+        initMap()
     }
+
+    @SuppressLint("MissingPermission")
+    fun initMap(){
+        mapView = MapView(this)
+        initPermission()
+        getCurLoc()
+        mapView.zoomIn(false)
+        mapView.zoomOut(false)
+
+        map_view.addView(mapView)
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading
+        mapView.setCurrentLocationEventListener(object :MapView.CurrentLocationEventListener{
+            override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+               // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onCurrentLocationUpdate(p0: MapView?, p1: MapPoint?, p2: Float) {
+               // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                getCurLoc()
+            }
+
+            override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+               // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+               // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
+    }
+
+    fun getCurLoc(){
+        if(checkAppPermission(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION))) {
+
+            val lm = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+            val providers = lm!!.getProviders(true)
+            var location: android.location.Location? = null
+            for (provider in providers) {
+                val l = lm!!.getLastKnownLocation(provider) ?: continue
+                if (location == null || l.getAccuracy() < location!!.getAccuracy()) {
+                    // Found best last known location: %s", l);
+                    location = l
+                }
+            }
+            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(location!!.latitude,location!!.longitude),true)
+            var marker = MapPOIItem() as MapPOIItem
+            marker.itemName = "나!"
+            marker.tag = 0
+            marker.markerType = MapPOIItem.MarkerType.YellowPin
+            marker.mapPoint = MapPoint.mapPointWithGeoCoord(location!!.latitude,location!!.longitude)
+            mapView.addPOIItem(marker)
+        }
+        else{
+            initPermission()
+        }
+    }
+
+
+    fun checkAppPermission(requestPermission:Array<String>):Boolean{
+        val requestResult = BooleanArray(requestPermission.size)
+        for (i in requestResult.indices){
+            requestResult[i] = ContextCompat.checkSelfPermission(this,requestPermission[i])==PackageManager.PERMISSION_GRANTED
+            if(!requestResult[i]){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun askPermission(requestPermission:Array<String>,REQ_PERMISSION:Int){
+        ActivityCompat.requestPermissions(this,requestPermission,REQ_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            LOCATION_REQUEST->{
+                if(checkAppPermission(permissions)){
+                    Toast.makeText(applicationContext,"위치 권한을 허락합니다",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(applicationContext,"위치 권한이 취소되었습니다",Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    fun initPermission(){
+        if(checkAppPermission(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION))){
+            Toast.makeText(this,"현재 위치를 사용합니다",Toast.LENGTH_SHORT).show()
+        }else{
+            askPermission(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_REQUEST)
+        }
+    }
+
 }
